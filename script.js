69cab1a7c44a22f8fe11eab765f0462ad2d18bbc
Andres Roldan <dresroldan@gmail.com>
2021-11-17 14:24:23 -0600

    Feat: add item counter ffunctions for adding and removing from cart

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navbar = document.querySelector('.navbar');
 const btnHamburger = document.querySelector('.hamburger');
 const btnMenuClose = document.querySelector('.navbar__btn');
+const btnPlusIcon = document.querySelector('.count__icon--plus');
+const btnMinusIcon = document.querySelector('.count__icon--minus');
+const counter = document.querySelector('.counter');
+
+let count = 0;
 
 const onHamburgerClick = () => {
   navbar.classList.remove('navbar--hidden');
@@ -10,5 +15,21 @@ const onMenuCloseClick = () => {
   navbar.classList.add('navbar--hidden');
 };
 
+const addItemcount = () => {
+  count++;
+  counter.innerHTML = count;
+};
+
+const removeItemcount = () => {
+  if (count >= 1) {
+    count--;
+    counter.innerHTML = count;
+  } else {
+    return count;
+  }
+};
+
 btnHamburger.addEventListener('click', onHamburgerClick);
 btnMenuClose.addEventListener('click', onMenuCloseClick);
+btnPlusIcon.addEventListener('click', addItemcount);
+btnMinusIcon.addEventListener('click', removeItemcount);
