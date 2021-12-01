df746f99d46c17245c49aa2515cd8d961edd1572
Andres Roldan <dresroldan@gmail.com>
2021-11-30 18:21:25 -0600

    Feat: product display in shopping cart when user adds product and then click on cart icon in header

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+// navbar
 const navbar = document.querySelector('.navbar');
 const btnHamburger = document.querySelector('.hamburger');
 const btnMenuClose = document.querySelector('.navbar-button');
-const btnPlusIcon = document.querySelector('.count-icon--plus');
-const btnMinusIcon = document.querySelector('.count-icon--minus');
-const btnAddtoCart = document.querySelector('.addcart-button');
-const counter = document.querySelector('.counter');
+
+// shopping cart in header
+const cartMenu = document.querySelector('.cart');
+const btnCart = document.querySelector('.cart-button');
 const cartCounter = document.querySelector('.cart-count');
+const cartMessage = document.querySelector('.cart-message');
+const productsInCartTotalPrice = document.querySelector('.products-in-cart');
+const btnDelete = document.querySelector('.delete-button');
+
+// product hero & slideshow gallery
 const btnNext = document.querySelector('.next');
 const btnPrev = document.querySelector('.previous');
 const gallery = document.querySelectorAll('.thumb-img');
 const heroImg = document.querySelector('.product-hero');
 
-const btnCart = document.querySelector('.cart-button');
-const cartMenu = document.querySelector('.cart');
+// add to shopping cart counter and button
+const btnPlusIcon = document.querySelector('.count-icon--plus');
+const btnMinusIcon = document.querySelector('.count-icon--minus');
+const btnAddtoCart = document.querySelector('.addcart-button');
+const counter = document.querySelector('.counter');
 
-const cartMessage = document.querySelector('.cart-message');
-
+// numerical values
 let countValue = 0;
 let productsInCart = 0;
+let price = 250.0;
+let discount = 0.5;
 
 const onHamburgerClick = () => {
   navbar.classList.remove('navbar--hidden');
@@ -44,12 +54,38 @@ const setProductCounter = (value) => {
     countValue += value;
     counter.innerHTML = countValue;
   }
-  console.log(countValue);
 };
 
 const addItemToCart = () => {
   productsInCart += countValue;
 
+  const productHTML = `   
+  <div class="item">
+      <img class="product-img" src="./images/image-product-1.jpg" alt="sneaker product"/>
+      <div class="product-details">
+          <div class="product-name">Autumn Limited Edition...</div>
+          <div class="price-group">
+            <div class="cart-price">$125.00</div> x
+            <div class="count">${productsInCart}</div>
+            <div class="total-amount">${(
+              price *
+              discount *
+              productsInCart
+            ).toFixed(2)}</div>
+    </div>
+    </div>
+    <img class="delete-button" src="./images/icon-delete.svg" alt="icon delete">
+  </div>
+  
+  `;
+
+  productsInCartTotalPrice.innerHTML = productHTML;
+
+  updateCart();
+};
+
+const onBtnDeleteClick = () => {
+  productsInCart--;
   updateCart();
 };
 
@@ -92,3 +128,4 @@ btnPlusIcon.addEventListener('click', addItemcount);
 btnMinusIcon.addEventListener('click', removeItemcount);
 btnAddtoCart.addEventListener('click', addItemToCart);
 btnCart.addEventListener('click', openCartMenu);
+btnDelete.addEventListener('click', onBtnDeleteClick);
